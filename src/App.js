4aa1be60becfa6ef36ec846f0bac9d82bfe546f9
SalgadoS <99090484+SagladoS@users.noreply.github.com>
2022-05-23 04:00:36 -0500

    api giphy

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import ListOfGifs from './components/ListOfGifs';
+import { Link, Route } from "wouter";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+      <section className="App-content">
+        <h1>App</h1>
+        <Link to='/search/panda'>Gifs de pandas</Link>
+        <Link to='/search/ecuador'>Gifs de ecuador</Link>
+        <Link to='/search/Chile'>Gifs de Chile</Link>
+        <Route 
+          component={ListOfGifs}
+          path='/search/:keyword' />
+      </section>
     </div>
   );
 }
